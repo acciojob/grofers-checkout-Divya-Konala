09bc33e10035a4709ebfb1fc610dc3987e812603
Divya-Konala <87460968+Divya-Konala@users.noreply.github.com>
2023-02-19 17:37:49 +0530

    grofers-checkout

--- script.js
+++ script.js
@@ -4,7 +4,19 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+let prices=document.querySelectorAll(".price");
+let totalPrice=0;
+  for(let i=0;i<prices.length;i++){
+    totalPrice+=Number(prices[i].innerHTML);
+  }
+  console.log(totalPrice);
+  let row=document.createElement("tr");
+  let td1=document.createElement("td");
+  td1.textContent="Total Price";
+  let td2=document.createElement("td");
+  td2.textContent=totalPrice;
+  row.append(td1,td2);
+  document.querySelector("table").append(row);
 };
 
 getSumBtn.addEventListener("click", getSum);
